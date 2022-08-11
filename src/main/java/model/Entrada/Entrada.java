5ef1fa977fad3282e5ad815e62b39b80b5c3e3ab
ElielBloemer <ebloemercorrea@frba.utn.edu.ar>
2022-08-11 20:38:03 -0300

    jugador finalizada

--- src/main/java/model/Entrada/Entrada.java
+++ src/main/java/model/Entrada/Entrada.java
@@ -1,5 +1,41 @@
 package model.Entrada;
 
+import model.Descuento.DescuentoMayor;
+import model.Descuento.DescuentoMedio;
+import model.Descuento.DescuentoMenor;
+import model.Usuario.Usuario;
+
+import java.time.LocalDate;
+
 public class Entrada {
 
+    //TODO nao se esqueca de instanciar!
+    private Usuario usuario;
+
+    private Evento evento;
+
+    private Float precio;
+
+
+
+    private DescuentoMayor descuentoMayor = new DescuentoMayor();
+
+    private DescuentoMedio descuentoMedio = new DescuentoMedio();
+
+    private DescuentoMenor descuentoMenor = new DescuentoMenor();
+
+
+  /*  public Void verificarDescuento(){
+
+        if(descuentoMayor.calcularDescuento(this)){
+            this.
+
+        } else if (descuentoMedio.calcularDescuento(this)) {
+
+        } else if (descuentoMenor.calcularDescuento(this)) {
+
+        }else{
+
+        }
+    }*/
 }
