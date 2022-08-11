742d2b5d42bd6c094dc1a00c45dd88f3ac876547
Agustín De Sousa Mariano <agus.desousa99@gmail.com>
2022-08-11 20:47:35 -0300

    Changes by EBloemer

--- src/main/java/model/Entrada/Jugador.java
+++ src/main/java/model/Entrada/Jugador.java
@@ -1,29 +1,4 @@
 package model.Entrada;
 
 public class Jugador {
-
-    private int id;
-
-    private String nombre;
-
-    public Jugador(int id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
