3fa2282ca62298b529f7acf9d3d89c1e761377a5
ElielBloemer <ebloemercorrea@frba.utn.edu.ar>
2022-08-12 03:15:23 -0300

    implementando uso APi..

---
+++ src/main/java/model/apiCotizacion/Cotizacion.java
@@ -0,0 +1,66 @@
+package model.apiCotizacion;
+
+import java.io.IOException;
+
+public class Cotizacion {
+
+    private int id;
+    private String fecha;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCompra(String compra) {
+        this.compra = compra;
+    }
+
+    public void setVenta(String venta) {
+        this.venta = venta;
+    }
+
+    private String compra;
+    private String venta;
+
+    private APICotizacion apiCotizacion = APICotizacion.getInstance();
+
+    public int getId() {
+        return id;
+    }
+
+    public Cotizacion(){ }
+
+    public Cotizacion(int id, String fecha, String compra, String venta){
+        this.id = id;
+        this.fecha = fecha;
+        this.compra = compra;
+        this.venta = venta;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getCompra(){
+        System.out.println(" compra seteada:" + this.compra);
+        return compra;
+    }
+
+    public double getVenta(){
+        return Double.parseDouble(venta);
+    }
+
+    public String toString(){
+        return "Cotizacion dolar blue\n\n" + "Fecha: "+this.fecha+"\nDolar venta: "+this.venta
+                +"\tDolar compra: "+this.compra +"\n\n"+" el valor del en la cual debes tomar es: "+this.compra;
+    }
+
+
+    public Cotizacion obtenerCotizacion() throws IOException {
+        return apiCotizacion.getCotizacion();
+    }
+}
